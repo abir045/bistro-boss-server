3f62fbd1ef1d09533b2b7b9b0da91ee93a2f688e
unknown <abir045@gmail.com>
2025-01-16 13:47:47 +0600

    updated server with order-stats route

--- index.js
+++ index.js
@@ -275,7 +275,7 @@ async function run() {
           },
         ])
         .toArray();
-      const revenue = result.length > 0 ? result[0].totalRevenue : 0;
+      const revenue = result.length > 0 ? result[0].totalRevenue.toFixed(2) : 0;
 
       res.send({
         users,
@@ -284,6 +284,61 @@ async function run() {
         revenue,
       });
     });
+
+    //order status
+    //using aggregate pipeline
+    app.get("/order-stats", verifyToken, verifyAdmin, async (req, res) => {
+      const result = await paymentCollection
+        .aggregate([
+          {
+            $unwind: "$menuItemIds",
+          },
+          {
+            $lookup: {
+              from: "menu",
+              localField: "menuItemIds",
+              foreignField: "_id",
+              as: "menuItems",
+            },
+          },
+          {
+            $unwind: "$menuItems",
+          },
+          {
+            $group: {
+              _id: "$menuItems.category",
+              quantity: {
+                $sum: 1,
+              },
+              revenue: {
+                $sum: "$menuItems.price",
+              },
+            },
+          },
+          {
+            $project: {
+              _id: 0,
+              category: "$_id",
+              quantity: "$quantity",
+              revenue: "$revenue",
+            },
+          },
+        ])
+        .toArray();
+
+      res.send(result);
+    });
+
+    /**
+     *
+     * -------------
+     * non efficient way
+     * 1. load all  payments
+     * 2. for every menuItemIds (which is an array), go find the
+     * item from menu collection
+     * 3. for every item in the menu collection that you found from  a payment entry
+     *
+     */
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
